bae3838dac89704f4eac21eebe78adbdfe84b0e8
hahuge <1240697048@qq.com>
2020-03-26 13:16:47 +0800

    测试git

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("master");
+        System.out.println("debug");
+        System.out.println("test");
     }
 }
